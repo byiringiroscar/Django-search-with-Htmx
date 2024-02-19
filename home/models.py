4635ce98453d6b71c2031dacd5528d6e16172357
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-02-19 10:16:38 +0200

    add view func

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.template.loader import render_to_string
+from django.http import JsonResponse
+from .models import Person
 
-# Create your views here.
+
+def search_view(request):
+    all_people = Person.objects.all()
+    context = {'count': all_people.count()}
+    return render(request, 'search.html', context)
+
+
+def search_results_view(request):
+    query = request.GET.get('search', '')
+    print(f'{query = }')
+
+    all_people = Person.objects.all()
+    if query:
+        people = all_people.filter(name__icontains=query)
+    else:
+        people = []
+
+    context = {'people': people, 'count': all_people.count()}
+    return render(request, 'search_results.html', context)
