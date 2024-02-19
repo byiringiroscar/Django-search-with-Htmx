efa7370524ab15d7c99a6def94659ab2f259e401
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-02-19 10:19:48 +0200

    add temp

--- home/urls.py
+++ home/urls.py
@@ -1,1 +1,8 @@
+# urls.py in sim
 from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('search/', views.search_view, name='search_view'),
+    path('search/results/', views.search_results_view, name='search_results_view'),
+]
