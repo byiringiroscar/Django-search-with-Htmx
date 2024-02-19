f983446035b692bba7b3ae826624bc13c453dce7
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-02-19 10:48:08 +0200

    Add style

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
+# views.py
+
 from django.shortcuts import render
-from django.template.loader import render_to_string
-from django.http import JsonResponse
 from .models import Person
+from django.utils.html import format_html
 
 
 def search_view(request):
@@ -17,8 +18,22 @@ def search_results_view(request):
     all_people = Person.objects.all()
     if query:
         people = all_people.filter(name__icontains=query)
+        highlighted_people = [{'name': highlight_matched_text(person.name, query), 'description': person.description}
+                              for person in people]
     else:
-        people = []
+        highlighted_people = []
 
-    context = {'people': people, 'count': all_people.count()}
+    context = {'people': highlighted_people, 'count': all_people.count()}
     return render(request, 'search_results.html', context)
+
+
+def highlight_matched_text(text, query):
+    """
+    Inserts html around the matched text.
+    """
+    start = text.lower().find(query.lower())
+    if start == -1:
+        return text
+    end = start + len(query)
+    highlighted = format_html('<span class="highlight">{}</span>', text[start:end])
+    return format_html('{}{}{}', text[:start], highlighted, text[end:])
